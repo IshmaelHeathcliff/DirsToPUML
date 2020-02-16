41f6cc14806456e7bf32431c6f8c88457a28f1b5
Ishmael <darkflare1055@gmail.com>
2020-02-16 13:37:02 +0800

    init

---
+++ DirsToPUML/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DirsToPUML
+{
+    internal class Program
+    {
+        static StringBuilder _puml = new StringBuilder();
+        static bool _isRoot = true;
+        static List<string> _notPrintExtensionsOrDirs = new List<string>();
+        static int _level = 1;
+
+        public static void DirToPuml(string dir)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dir);
+
+            if (_isRoot)
+            {
+                _puml.Append($"@startmindmap\n* {dirInfo.Name}\n");
+                _isRoot = false;
+                _level++;
+            }
+            
+            FileSystemInfo[] subDirsAndFilesInfo = dirInfo.GetFileSystemInfos();
+            string symbol = new string('*', _level);
+            
+            foreach (FileSystemInfo FSInfo in subDirsAndFilesInfo)
+            {
+                if (FSInfo is DirectoryInfo && !_notPrintExtensionsOrDirs.Contains(FSInfo.Name))
+                {
+                    _puml.Append( symbol + $" {FSInfo.Name}\n");
+                    _level++;
+                    DirToPuml(FSInfo.FullName);
+                }
+                else
+                {
+                    bool isPrinted = true;
+                    foreach (string ex in _notPrintExtensionsOrDirs)
+                    {
+                        if (FSInfo.Name.EndsWith(ex))
+                        {
+                            isPrinted = false;
+                            break;
+                        }
+                    }
+                    if (isPrinted) _puml.Append(symbol + $"_ {FSInfo.Name}\n");
+                }
+            }
+
+            _level--;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the directory:");
+            string targetDir = Console.ReadLine();
+            Console.WriteLine("Please input the extensions or directories not printed with spaces as gaps(for example: .git .sln .meta):");
+            _notPrintExtensionsOrDirs = new List<string>(Console.ReadLine().Split(' '));
+            DirToPuml(targetDir);
+            _puml.Append("@endmindmap");
+            StreamWriter writer = new StreamWriter("out.puml");
+            using (writer)
+            {
+                writer.Write(_puml.ToString());
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# 介绍
+
+将目录下的文件夹和文件以mindnode的形式输出到out.puml
+
+# 使用方式
+
+1. 下载整个库
+2. 打开DirsToPUML.sln并build（建议使用Rider）
+3. 进入DirsToPUML\bin\Debug\
+4. 双击打开DirsToPUML.exe，或将文件复制到其他地方再打开
+5. 输入目标目录，相对路径或绝对路径都可以
+6. 输入不想要输出的文件夹名、文件名或文件扩展名
+7. 打开out.puml（建议使用vscode并安装plantuml扩展）
